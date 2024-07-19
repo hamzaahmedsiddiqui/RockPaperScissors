cd4c58b067640bfb6dd4b3a43b1239297d3268bd
hamzaahmedsiddiqui <dev.hamxa@gmail.com>
2024-07-19 12:34:58 +0200

    Inital commit

---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by hamza Ahmed on 2024-07-19.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
